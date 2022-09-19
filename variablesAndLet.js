c2d751f22d7611c2294d2bd7541a8c04533db01f
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-20 02:25:18 +0600

    wright some let variable

---
+++ variablesAndLet.js
@@ -0,0 +1,14 @@
+let someName = "Value";
+let age = 72;
+let raining = true;
+let number = 12;
+let number1 = 23;
+
+let plus = number + number1;
+console.log(plus);
+
+let dethOfBirth = 2000;
+let years = 2022;
+
+let age1 = years - dethOfBirth;
+console.log(age1);
