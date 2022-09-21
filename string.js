f190cc35bac2d9d3dbf230053ffdced9c87adc5c
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-21 13:09:55 +0600

    right some string variable

---
+++ string.js
@@ -0,0 +1,31 @@
+const str = "sazzad";
+console.log(str);
+
+// const str1 = " ha ha ha '; // it will not work
+//Right way//
+const str1 = "ha ha ha";
+console.log(str1);
+
+const firstName = "Sazzad";
+const lastName = "Tomal";
+
+const name = firstName + " " + lastName;
+console.log(name);
+
+const nameLength = name.length;
+console.log(nameLength);
+
+const text = "It's only mine";
+console.log(text);
+
+const upercase = text.toUpperCase();
+console.log(upercase);
+
+const lowerCase = upercase.toLocaleLowerCase();
+console.log(lowerCase);
+
+const color = "purple                 ";
+color.trim().toUpperCase();
+console.log(color);
+
+// 3 er 4 ses 5 start //
--- variablesAndLet.js
+++ variablesAndLet.js
@@ -12,3 +12,35 @@ let years = 2022;
 
 let age1 = years - dethOfBirth;
 console.log(age1);
+
+// const //
+
+const number2 = 70;
+// number2 = 55;
+// console.log(number2);
+// resust will show Error coase const will never changeable.that's why result will show Error//
+
+/////////////////////////////////
+let eggCount = 42;
+eggCount += 2;
+console.log(eggCount);
+//////////////////////////////////
+let wind_speed = 76;
+wind_speed += 5;
+wind_speed--;
+console.log(wind_speed);
+
+// boolian type //
+
+let isRaining = false;
+isRaining = true;
+console.log(raining);
+
+let isAngry = false;
+let jsIsEasy = true;
+console.log(jsIsEasy);
+
+let gameOverOfJs = false;
+console.log(gameOverOfJs);
+
+//////////////////////////////////
