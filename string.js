3601fc880de90951f6a1f09f6b91ae600aa6d6e9
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-04 03:04:06 +0600

    indexOf and slice method

--- string.js
+++ string.js
@@ -28,4 +28,44 @@ const color = "purple                 ";
 color.trim().toUpperCase();
 console.log(color);
 
-// 3 er 4 ses 5 start //
+console.log("------------ Start (indexOf) --------------");
+
+const a = "baseball".indexOf("ball");
+const b = "baseball".indexOf("b");
+const c = "Baseball".indexOf("b");
+const d = "baseball".indexOf("base");
+const e = "baseball".indexOf("e");
+const f = "baseball".indexOf("dfdfdfdf");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+console.log("------------ Start slice method --------------");
+
+let string = "superhero";
+let slice = string.slice(0, 5);
+console.log(slice);
+console.log("superhero".slice(0, 5));
+
+const nameSlice = "sazzadHossain";
+const getFirstName = nameSlice.slice(0, 5);
+console.log(getFirstName);
+
+const getLastName = nameSlice.slice(6, 13).toUpperCase().slice(0, 1);
+console.log(getLastName);
+
+const getLastNameLetter = nameSlice.slice(6, 13).slice(1, 6);
+console.log(getLastNameLetter);
+
+const addNameLetter = getLastName + getLastNameLetter;
+console.log(addNameLetter);
+
+//...................................
+
+const superhero = "superhero";
+const he = superhero.slice(5, 7);
+console.log(he);
+// 3 er 5 ses 6start //
