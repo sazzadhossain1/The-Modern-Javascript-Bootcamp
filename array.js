cd264bf346cc23f02d2eee7bc46aa469748fe4bc
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-09 18:29:19 +0600

    slice and splece method

--- array.js
+++ array.js
@@ -41,4 +41,59 @@ const drinks = ["AppleJuse", "OrangeJuice"];
 const fruitsAndVeggiesAndDrinks = [...fruitsAndVeggies.concat(drinks)];
 console.log(fruitsAndVeggiesAndDrinks);
 
-// 5 er 4 ses 5 start
+console.log("------------------- inclides ------------------");
+
+console.log(fruitsAndVeggiesAndDrinks.includes("OrangeJuice"));
+console.log(fruitsAndVeggiesAndDrinks.includes("komolaJuice"));
+
+console.log(fruits.includes("Apple"));
+console.log(veggies.includes("Brussel", 1));
+console.log(veggies.includes("Brussel", 5));
+
+console.log("------------- indexOf --------------");
+
+console.log(fruits.indexOf("Apple"));
+console.log(fruits.indexOf("Orange"));
+console.log(fruitsAndVeggiesAndDrinks.indexOf("Brussel"));
+
+console.log(fruits.indexOf("komola"));
+console.log(fruitsAndVeggies.indexOf("puishak"));
+console.log(fruitsAndVeggiesAndDrinks.indexOf("gendarir shorbot"));
+
+if (fruits.includes("Apple") && fruits[0] === "Apple") {
+  console.log("Give me a glass of Apple juice");
+} else {
+  console.log("No thanks Brow");
+}
+
+//---------------------------------------------------------------
+
+if (fruits.includes("Orange") && fruits.indexOf("Orange") !== -1) {
+  console.log("This is orange juice");
+} else {
+  console.log("This is not orange juice");
+}
+let letters = ["a", " b", "c", "d", "e", "f"];
+let le = letters.reverse().join("-").toUpperCase();
+
+console.log(le);
+console.log(typeof le);
+
+const animals = ["shark", "salmon", "whale", "bear", "lizard", "tortoise"];
+
+// const swimmers = animals.slice(0, 3);
+// console.log(swimmers);
+
+// const mammals = animals.slice(2, 4);
+// console.log(mammals);
+
+// animals.splice(1, 0, "Octopus");
+
+animals.splice(3, 1, 10);
+console.log(animals);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.splice(3, 3, 1111, 22222, 33333, 44444, 55555);
+console.log(numbers);
+// 5 er 7 ses 8 start
