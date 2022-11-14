cf39b889810abd5163444a727d009dce83c289b1
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-14 16:34:00 +0600

    argument and perameter in function

--- function.js
+++ function.js
@@ -12,3 +12,23 @@ grumpus();
 for (let i = 0; i <= 10; i++) {
   grumpus(4444444444444);
 }
+
+function rollDice() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+
+function throwDice() {
+  rollDice();
+  rollDice();
+  rollDice();
+  rollDice();
+  rollDice();
+}
+
+throwDice();
+
+const name = function (nickName) {
+  console.log(`Hi ${nickName}`);
+};
+name("Sazzad");
