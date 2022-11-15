5fd7b7dcba73a48dfdf63bdad38d1d5ce372f6e2
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-16 01:34:29 +0600

    practice some function

--- function.js
+++ function.js
@@ -28,7 +28,156 @@ function throwDice() {
 
 throwDice();
 
-const name = function (nickName) {
+const name1 = function (nickName) {
   console.log(`Hi ${nickName}`);
 };
-name("Sazzad");
+name1("Sazzad");
+
+//.........................
+
+function square(num) {
+  console.log(num * num);
+}
+square(4);
+
+//.........................
+
+function sum(x, y) {
+  console.log(x + y);
+}
+sum(10, 20);
+
+//..........................
+
+const divided = (a, b) => {
+  return a / b;
+};
+console.log(divided(10, 2));
+
+//.....................................
+
+function isPurple(color) {
+  if (color.toLowerCase() === "purple") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPurple("purple"));
+
+//..........................................
+const color = ["blue", "pink", "bagenta"];
+function containsPurple(array) {
+  for (arr of array) {
+    if (color === "blue" || color === "pink") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+containsPurple(color);
+
+//...................................
+
+function isValidPassword(password, userName) {
+  if (password.length < 8) {
+    return false;
+  }
+  if (password.indexOf(" ") !== -1) {
+    return false;
+  }
+  if (password.indexOf(userName) !== -1) {
+    return false;
+  }
+  return true;
+}
+console.log(isValidPassword("dfdfdf434343434", "sazzad"));
+console.log(isValidPassword("dfd", "sazzad"));
+
+//.........................................
+
+function isValidPassword(password, userName) {
+  if (
+    password.length < 8 ||
+    password.indexOf(" ") !== -1 ||
+    password.indexOf(userName) !== -1
+  ) {
+    return false;
+  }
+  return true;
+}
+console.log(isValidPassword("dfdfdf434343434", "sazzad"));
+console.log(isValidPassword("dfd", "sazzad"));
+
+console.log("---------- Next Function Challange ---------");
+
+function avg(...arr) {
+  let total = 0;
+  for (let num of arr) {
+    total += num;
+  }
+  return total / arr.length;
+}
+console.log(avg(12, 11, 23, 33));
+console.log(avg(23, 108));
+
+console.log("------- Next another function challange ------");
+
+const isPangram = (sentence) => {
+  let toUpperCase = sentence.toUpperCase();
+  for (let char of "SKDFJKSADFKLSADF") {
+    if (toUpperCase.indexOf(char) === -1) {
+      return false;
+    }
+    return true;
+  }
+};
+
+console.log(isPangram("ZQXZQQQZXS"));
+
+//.............................................
+const isPangram1 = (sentence) => {
+  let toUpperCase = sentence.toUpperCase();
+  for (let char of "SKDFJKSADFKLSADF") {
+    if (!toUpperCase.includes(char)) {
+      return false;
+    }
+    return true;
+  }
+};
+
+console.log(isPangram1("qzqxmnlpo"));
+
+//........................................
+
+function getCard() {
+  const values = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "a",
+    "a",
+  ];
+  const idx = Math.floor(Math.random() * values.length);
+  console.log(idx);
+}
+getCard();
+
+function lol() {
+  let person = "Tom";
+  const age = 45;
+  var color1 = "teal";
+  console.log(person, age, color1);
+}
+lol();
+// 9 number er 1 number ses 2 number start
