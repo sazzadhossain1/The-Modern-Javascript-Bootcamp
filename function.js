5dcf8a091d2a880f83f4d08c4bd3f063d3992ed4
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-17 10:41:24 +0600

    higher order function

--- function.js
+++ function.js
@@ -272,4 +272,45 @@ console.log(functionObject.minusFunction(25, 15));
 console.log(functionObject.multiplyFunction(25, 15));
 console.log(functionObject.dividedFunction(20, 50));
 
+console.log("------------- Higher order function --------------");
+
+function callThreeTimes(f) {
+  f();
+  f();
+  f();
+}
+
+function cry() {
+  console.log("BOO HOO I'M SO SAD!");
+}
+function rage() {
+  console.log("I HAVE EVERYTHING !");
+}
+
+console.log(callThreeTimes(cry));
+console.log(callThreeTimes(rage));
+
+function repeatNTimes(action, num) {
+  for (let i = 0; i < num; i++) {
+    action();
+  }
+}
+
+repeatNTimes(rage, 13);
+repeatNTimes(cry, 15);
+
+console.log("------------------- xxxxx ------------------");
+
+function pickOne(f1, f2) {
+  let rand = Math.random();
+  console.log(rand);
+  if (rand < 0.5) {
+    f1();
+  } else {
+    f2;
+  }
+}
+console.log(pickOne(cry, rage));
+console.log(pickOne(rage, cry));
+
 // 9 number er 5 number ses 6 number start
