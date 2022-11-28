55dcd842320aa38744cba78afb96a96cbf27ee23
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-29 03:02:07 +0600

    practice some dom

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ for (let i = 0; i < inp.length; i++) {
   console.log(inp[i]);
 }
 
-const don = document.querySelectorAll("p");
+const don = document.querySelectorAll(".pp");
 console.log(don);
 
 console.log("--------- dom practice ----------");
@@ -55,4 +55,54 @@ console.log(getImg);
 
 const getOneLiTag = document.querySelector("body section ul li.special");
 console.log(getOneLiTag);
-// 13 number er 7 number ta ses 8 number ta start
+
+getImg.addEventListener("mouseover", function () {
+  this.style.width = "600px";
+  getImg.style.borderRadius = "50px";
+});
+
+getImg.addEventListener("mouseout", () => {
+  getImg.style.width = "300px";
+  getImg.style.borderRadius = "";
+});
+
+const h1Change = (hOne.innerText = " My First Damy Practice Website");
+
+console.log(hOne.innerText);
+console.log(document.body.innerText);
+console.log(document.body.innerHTML);
+
+const pMain = document.querySelector("#main");
+console.log(pMain);
+
+const getBTag = document.querySelector("b");
+console.log(getBTag);
+
+const getButton = document.querySelector(".btn-1");
+console.log(getButton);
+
+const getBtn = document.querySelector(".btn");
+// pMain.addEventListener("mouseover", () => {
+//   getBTag.style.display = "block";
+// });
+
+// pMain.addEventListener("mouseout", () => {
+//   getBTag.style.display = "none";
+// });
+
+// getButton.addEventListener("click", function () {
+//   getBTag.style.display = "block";
+// });
+getButton.addEventListener("click", function () {
+  getBTag.style.display = "block";
+  getButton.style.display = "none";
+  getBtn.style.display = "block";
+});
+
+getBtn.addEventListener("click", function () {
+  getBtn.style.display = "none";
+  getButton.style.display = "block";
+  getBTag.style.display = "none";
+});
+
+// 14 number er 0 number ta ses 1 number ta start
