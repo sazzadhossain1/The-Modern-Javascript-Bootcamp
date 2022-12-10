26c149ea1e54a8ae67635ee995efae6ab1ed9395
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-11 00:29:23 +0600

    constructor function

--- factoryFunction.js
+++ factoryFunction.js
@@ -24,4 +24,64 @@ console.log(firstColor.rgb());
 //         Codeinnoviro youtube channel           //
 ///////////////////////////////////////////////////
 
-// 19 number er 3 number ta ses 4 number ta start
+const circle = {
+  redius: 1,
+  position: {
+    x: 1,
+    y: 1,
+  },
+  isVisiting: true,
+  draw() {
+    console.log("draw");
+  },
+};
+console.log(circle.position.x);
+
+console.log("------------- Factory Function ------------");
+
+function createCircle(radius, position, isVisible) {
+  return {
+    radius,
+    position,
+    isVisible,
+    draw() {
+      console.log("draw is another function");
+    },
+  };
+}
+const cicle1 = createCircle(1, { x: 1, y: 2 }, true);
+console.log(cicle1);
+
+console.log("------------- Constructor Function ------------");
+
+function CreateCircle1(radius, position, isVisible) {
+  this.radius = radius;
+  this.position = position;
+  this.isVisible = isVisible;
+}
+console.log(new CreateCircle1(1, { x: 1, y: 2 }, true));
+
+// Another Example
+
+function Student(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = dob;
+}
+const student1 = new Student("Sazzad", "Hossain", "10 / 2 / 1996");
+console.log(student1.firstName);
+
+// Another Example - 1
+
+const Person = function (firName, lasName, dob) {
+  this.firName = firName;
+  this.lasName = lasName;
+  this.dob = new Date(dob);
+};
+
+const personeOne = new Person("Shayla", "Akther", "1/5/1995");
+console.log(personeOne);
+
+const fullName = `${personeOne.firName} ${personeOne.lasName} and she's Deth of Birth is ${personeOne.dob}`;
+console.log(fullName);
+// 19 number er 4 number ta ses 5 number ta start
